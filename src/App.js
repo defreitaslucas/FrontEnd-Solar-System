4cf24826c23098d261bda6b5eedb8bac3cf09f11
LucasTapa <lucas.dfa@live.com>
2022-01-25 15:18:14 -0300

    consertando o requisito 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
     return (
       <main>
         <Header />
-        <SolarSystem />
-        <Title headline="Planetas" />
+        <SolarSystem Titles/>
       </main>
 
     );
